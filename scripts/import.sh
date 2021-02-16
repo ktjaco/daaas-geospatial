81230447eb1600e65c06fd9dc94ce49c148745ac
ktjaco <kent.jacobs@gmail.com>
2021-02-16 14:52:02 -0500

    comment out french NRN data

--- scripts/import.sh
+++ scripts/import.sh
@@ -23,12 +23,12 @@ for shp in `ls -1 ../data/nrn*/NRN*/*/*.shp`;
 	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
 done
 
-for shp in `ls -1 ../data/nrn*/RRN*/*.shp`;	
-	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
-done
+# for shp in `ls -1 ../data/nrn*/RRN*/*.shp`;	
+# 	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
+# 	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
+# done
 
-for shp in `ls -1 ../data/nrn*/RRN*/*/*.shp`;	
-	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
-done
+# for shp in `ls -1 ../data/nrn*/RRN*/*/*.shp`;	
+# 	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
+# 	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
+# done
