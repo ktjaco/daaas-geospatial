b467f9f0168894d56436b5b1bf909e73c78422e3
ktjaco <kent.jacobs@gmail.com>
2021-02-16 14:39:59 -0500

    psql fix

--- scripts/import.sh
+++ scripts/import.sh
@@ -10,25 +10,25 @@
 # import STC boundary files into PostGIS
 for shp in `ls -1 ../data/*/*.shp`;	
 	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$HOST --port=$PORT --username=$USER --dbname=$DB_NAME
+	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
 done
 
 for shp in `ls -1 ../data/nrn*/NRN*/*.shp`;	
 	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$HOST --port=$PORT --username=$USER --dbname=$DB_NAME
+	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
 done
 
 for shp in `ls -1 ../data/nrn*/NRN*/*/*.shp`;	
 	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$HOST --port=$PORT --username=$USER --dbname=$DB_NAME
+	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
 done
 
 for shp in `ls -1 ../data/nrn*/RRN*/*.shp`;	
 	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$HOST --port=$PORT --username=$USER --dbname=$DB_NAME
+	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
 done
 
 for shp in `ls -1 ../data/nrn*/RRN*/*/*.shp`;	
 	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$HOST --port=$PORT --username=$USER --dbname=$DB_NAME
+	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
 done
