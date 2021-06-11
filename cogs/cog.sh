a89866204b5fcc2fe45eb1808b591b5cf3ab9c57
ktjaco <kent.jacobs@gmail.com>
2021-06-10 20:45:27 -0400

    rename folder

--- cogs/cog.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for img in `ls -1 *.tif`;
-	do gdal_translate ${img%%.*}.tif ${img%%.*}_cog.tif -of COG -co COMPRESS=LZW
-done
--- cogs/geotiff.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for img in `ls -1 .`;
-	do gdal_translate -of GTiff $img ${img%%.*}.tif
-done
