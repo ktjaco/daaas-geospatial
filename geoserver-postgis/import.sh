034297a618b3eb2df00b22df8d1a63dce2799b3c
Kent Jacobs <14288239+ktjaco@users.noreply.github.com>
2021-07-02 14:45:29 -0400

    Update import.sh

--- geoserver-postgis/import.sh
+++ geoserver-postgis/import.sh
@@ -8,7 +8,7 @@
 # export PGPASSWORD=geoadm
 
 # import STC boundary files into PostGIS
-for shp in `ls -1 ../data/*/*.shp`;	
+for shp in `ls -1 ../data/l*/*.shp`;	
 	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
 	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
 done
@@ -22,6 +22,11 @@ done
 for shp in `ls -1 ../data/nrn*/NRN*/NRN*/*.shp`;	
 	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
 	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
+done
+
+for shp in `ls -1 ../data/ne*/*.shp`;
+	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
+	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=natearth
 done
 
 # for shp in `ls -1 ../data/nrn*/RRN*/*.shp`;	
