ccedd8c197ab13b1f9a4c8513cfabaed465b9636
Kent Jacobs <14288239+ktjaco@users.noreply.github.com>
2021-07-08 16:27:38 -0400

    Update import-data.sh

--- build/scripts/import-data.sh
+++ build/scripts/import-data.sh
@@ -1,42 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
 # Environment variables.
 source credentials.sh
 
 # Import STC boundary files into PostGIS.
-for shp in `ls -1 $DATA_DIR/l*/*.shp`;	
-	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$PG_HOST --port=$PG_PORT --username=$PG_USERNAME --dbname=sdi
+for shp in `ls -1 $DATA_DIR/l*/*.shp`;
+do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
+psql --host=$PG_HOST --port=$PG_PORT --username=$PG_USERNAME --dbname=$PG_SDI_DB
 done
 
-for shp in `ls -1 $DATA_DIR/g*/*.shp`;	
-	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$PG_HOST --port=$PG_PORT --username=$PG_USERNAME --dbname=sdi
+for shp in `ls -1 $DATA_DIR/g*/*.shp`;
+do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
+psql --host=$PG_HOST --port=$PG_PORT --username=$PG_USERNAME --dbname=$PG_SDI_DB
 done
 
 # Import NRN files into PostGIS.
-for shp in `ls -1 $DATA_DIR/nrn*/NRN*/*.shp`;	
-	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$PG_HOST --port=$PG_PORT --username=$PG_USERNAME --dbname=nrn
+for shp in `ls -1 $DATA_DIR/nrn*/NRN*/*.shp`;
+do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
+psql --host=$PG_HOST --port=$PG_PORT --username=$PG_USERNAME --dbname=$PG_NRN_DB
 done
 
-for shp in `ls -1 $DATA_DIR/nrn*/NRN*/NRN*/*.shp`;	
-	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$PG_HOST --port=$PG_PORT --username=$PG_USERNAME --dbname=nrn
+for shp in `ls -1 $DATA_DIR/nrn*/NRN*/NRN*/*.shp`;
+do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
+psql --host=$PG_HOST --port=$PG_PORT --username=$PG_USERNAME --dbname=$PG_NRN_DB
 done
 
-for shp in `ls -1 $DATA_DIR/nrn*/RRN*/*.shp`;	
-	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
+for shp in `ls -1 $DATA_DIR/nrn*/RRN*/*.shp`;
+do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
+psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$PG_NRN_DB
 done
 
-for shp in `ls -1 $DATA_DIR/nrn*/RRN*/*/*.shp`;	
-	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$DB_NAME
+for shp in `ls -1 $DATA_DIR/nrn*/RRN*/*/*.shp`;
+do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
+psql --host=$DB_HOST --port=$PORT --username=$USER --dbname=$PG_NRN_DB
 done
 
 # Import Natural Earth files into PostGIS.
 for shp in `ls -1 $DATA_DIR/ne*/*.shp`;
-	do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
-	psql --host=$PG_HOST --port=$PG_PORT --username=$PG_USERNAME --dbname=natearth
+do shp2pgsql -W latin1 -s 3347 -g geom -I -D $shp `basename $shp .shp` | \
+psql --host=$PG_HOST --port=$PG_PORT --username=$PG_USERNAME --dbname=$PG_NAT_DB
 done
