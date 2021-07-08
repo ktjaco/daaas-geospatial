2efb1db87e5d3484587025324446e3cbc986d636
Kent Jacobs <14288239+ktjaco@users.noreply.github.com>
2021-07-08 16:33:28 -0400

    Update enable-postgis.sh

--- build/scripts/enable-postgis.sh
+++ build/scripts/enable-postgis.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
 
+# Environment variables.
+source credentials.sh
+
+# Create the PostGIS extension for NRN.
 psql \
     --host=$PG_HOST \
     --username=$PG_USERNAME \
     --port=$PG_PORT \
-    --dbname=nrn \
+    --dbname=$PG_NRN_DB \
     -c "CREATE EXTENSION postgis;"
 
+# Create the PostGIS extension for SDI.
 psql \
     --host=$PG_HOST \
     --username=$PG_USERNAME \
     --port=$PG_PORT \
-    --dbname=sdi \
+    --dbname=$PG_SDI_DB \
     -c "CREATE EXTENSION postgis;"
 
+# Create the PostGIS extension for NATEARTH.
 psql \
     --host=$PG_HOST \
     --username=$PG_USERNAME \
     --port=$PG_PORT \
-    --dbname=natearth \
+    --dbname=$PG_NAT_DB \
     -c "CREATE EXTENSION postgis;"
