f6f8f9bf1bbbfc1883995b03fbfdf59aafebbfd0
Kent Jacobs <14288239+ktjaco@users.noreply.github.com>
2021-07-08 16:19:20 -0400

    Update drop-database.sh

--- build/scripts/drop-database.sh
+++ build/scripts/drop-database.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
-psql \
-    --host=$PG_HOST \
-    --username=$PG_USERNAME \
-    --port=$PG_PORT \
-    -c "DROP DATABASE nrn;"
+source credentials.sh
 
 psql \
     --host=$PG_HOST \
     --username=$PG_USERNAME \
     --port=$PG_PORT \
-    -c "DROP DATABASE sdi;"
+    --dbname='postgres' \
+    -c "DROP DATABASE $PG_NRN_DB;"
 
 psql \
     --host=$PG_HOST \
     --username=$PG_USERNAME \
     --port=$PG_PORT \
-    -c "DROP DATABASE natearth;"
+    --dbname='postgres' \
+    -c "DROP DATABASE $PG_SDI_DB;"
+
+psql \
+    --host=$PG_HOST \
+    --username=$PG_USERNAME \
+    --port=$PG_PORT \
+    --dbname='postgres' \
+    -c "DROP DATABASE $PG_NAT_DB;"
